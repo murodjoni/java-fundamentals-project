9925bdddcb798c10cec930958547abdd0ac17fc9
Murodjoni Sharifzoda <murodjoni@aol.com>
2024-04-13 11:13:15 -0600

    For Loop practice

--- ForLoops.java
+++ ForLoops.java
@@ -1,5 +1,27 @@
+import java.util.Random;
+
 public class ForLoops{
     public static void main(String[] args) {
-        System.out.println("This is a ForLoops test");
+        //Printing text 9 times
+        for(int i = 0; i < 9; i++){
+            System.out.println("I think, therefore I am");
+        }
+        //Printing all numbers 0-4
+        for(int i = 0; i <= 4; i++){
+            System.out.println(i);
+        }
+        
+        //Printing numbers included in an array
+        int[] ages = {0, 1, 2, 7, 8, 9};
+        for( int i = 0; i < ages.length; i++){
+            System.out.println("Number: " + ages[i]);
+        }
+
+        Random rand = new Random();
+
+        //Printing random number up to 15, (not including 15)
+        int randomNum = rand.nextInt(15);
+        System.out.println("The random num from array is: " + randomNum);
+        
     }
 }
