e36ec8726639a92b43cdac6d38886839dbc8ce73
Murodjoni Sharifzoda <murodjoni@aol.com>
2024-04-14 12:16:19 -0600

    Calling methods inside our main method

---
+++ Methods.java
@@ -0,0 +1,17 @@
+public class Methods {
+    public static void main(String[] args) {
+        welcome();
+        //Printing a * b using the Multiply method
+        multiply(6, 6);
+        divide(20, 5);
+    }
+    public static void welcome(){
+        System.out.println("Welcome to our calculator!");
+    }
+    public static void multiply(int a, int b){
+        System.out.println("Using multiply method: " + a * b);
+    }
+    public static void divide(int a, int b){
+        System.out.println("Using divide method: " + a / b);
+    }
+}
