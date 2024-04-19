b1bdb3c345a959766e2d4e52533aa229d1aab98a
Murodjoni Sharifzoda <murodjoni@aol.com>
2024-04-19 14:34:45 -0600

    Started while loops

---
+++ WhileLoops.java
@@ -0,0 +1,12 @@
+public class WhileLoops {
+    public static void main(String[] args) {
+        welcome();
+        myWhileLoop();
+    }
+    public static void welcome(){
+        System.out.println("Welcome to the While Loop practice!");
+    }
+    public static void myWhileLoop(){
+        System.out.println("We are going to practice while loops here! ");
+    }
+}
