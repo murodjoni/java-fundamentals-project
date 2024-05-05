f3b0b1fdce5952f6c1bc019fe85ea62e7d22366d
Murodjoni Sharifzoda <murodjoni@Murodjonis-MacBook-Pro.local>
2024-05-05 17:27:28 -0600

    Changed output format

--- Arrays.java
+++ Arrays.java
@@ -1,25 +1,30 @@
 public class Arrays {
     public static void main(String[] args) {
 
-        System.out.println("Welcome to your local grocery store!");
+        Grocery();
+    }
+
+        public static void Grocery(){
+        System.out.println("");
+        System.out.println("Welcome to your local grocery store! \n");
         String groceryBag[] = {"Toilet Paper", "toothbrush", "water", "bananas", "bread", "coffee"};
         String paymentMethods[] = {"MasterCard", "Visa", "Cash", "ApplePay", "Crypto"};
-        int invalidCashNotes[] = {1, 20};
-        System.out.println("Cash notes not accepted at our Grocery Store: " + "$" + invalidCashNotes[0] + " dollar bills and $" + invalidCashNotes[1] + " dollar bills");
+        int invalidCashNotes[] = {50, 100};
+        System.out.println("Cash notes that are not accepted at our Grocery Store: " + "$" + invalidCashNotes[0] + " bills and $" + invalidCashNotes[1] + " bills. \n");
 
         System.out.println("Available grocery items: ");
         System.out.println(groceryBag[0]);
         System.out.println(groceryBag[1]);
         System.out.println(groceryBag[2]);
         System.out.println(groceryBag[3]);
+        System.out.println(groceryBag[4]);
+        System.out.println(groceryBag[5] + "\n");
 
         System.out.println("These are the accepted payment methods: ");
         System.out.println(paymentMethods[0]);
         System.out.println(paymentMethods[1]);
         System.out.println(paymentMethods[2]);
         System.out.println(paymentMethods[3]);
-        System.out.println(paymentMethods[4]);
-
-        System.out.println("Printing line to test Github connection");
-    }
+        System.out.println(paymentMethods[4] + "\n");
+        }
 }
