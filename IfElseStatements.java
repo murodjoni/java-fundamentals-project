8390bd048548bca222d113dc6289e44db7a4e427
Murodjoni Sharifzoda <murodjoni@Murodjonis-MacBook-Pro.local>
2024-05-05 17:08:47 -0600

    Added a while loop

--- IfElseStatements.java
+++ IfElseStatements.java
@@ -3,17 +3,18 @@ import java.util.Scanner;
 public class IfElseStatements {
     public static void main(String[] args) {
         Scanner scan = new Scanner(System.in);
-        
+        while(true){
+        System.out.println("");
         System.out.println("Enter one of the following Fruit or Vegetable items and I will tell you if it's a Fruit, Vegetable or Neither: ");
-
-        System.out.println("orange, grapes, apple, potato, tomato, garlic, carrot, onion, pepper, phone, lamp, pencil, table, chair");
+        System.out.println("");
+        System.out.println("orange, grapes, apple, potato, tomato, garlic, carrot, onion, pepper, phone, lamp, pencil, table, chair, etc");
 
         String userInput = scan.nextLine();
-
+        
         //Using If statements to check if user entered fruit vegetable, or neither 
        if(userInput.equals("orange")){
         System.out.println(userInput + " is a fruit");
-       }else if(userInput.equals("grapes")){
+        }else if(userInput.equals("grapes")){
         System.out.println(userInput + " is a fruit");
         }else if(userInput.equals("apple")){
         System.out.println(userInput + " is a fruit");
@@ -32,6 +33,7 @@ public class IfElseStatements {
         }else{
             System.out.println(userInput+ " is neither Fruit nor Vegetable");
         }    
+        }
     }
        };
     
